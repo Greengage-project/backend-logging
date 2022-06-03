3384fac29aaf4bd947de6ed0a611c1e4a64ab8c3
Julen Badiola <badiolajulen@gmail.com>
2022-06-03 14:28:59 +0200

    Port as integer

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ from pydantic import BaseModel, Extra
 
 BASE_PATH = os.environ.get("BASE_PATH")
 ELASTICSEARCH_HOST = os.environ.get("ELASTICSEARCH_HOST", "elasticsearch")
-ELASTICSEARCH_PORT = os.environ.get("ELASTICSEARCH_PORT", 9200)
+ELASTICSEARCH_PORT = int(os.environ.get("ELASTICSEARCH_PORT", 9200))
 
 
 class LogsCreate(BaseModel, extra=Extra.allow):
