6b006558b8b3215a46f0f93aede2b12cf678accb
Julen Badiola <badiolajulen@gmail.com>
2022-06-20 13:32:46 +0200

    Without rabbit

--- app/main.py
+++ app/main.py
@@ -1,17 +1,11 @@
-import asyncio
 import json
 import logging
 import os
-import uuid
-from base64 import b64decode
 from datetime import datetime
 from sys import stdout
 from typing import List, Optional
 
-import aiormq
-from aiormq.abc import DeliveredMessage
 from elasticsearch import AsyncElasticsearch
-from elasticsearch.helpers import async_bulk
 from fastapi import FastAPI, Query
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel, Extra
@@ -55,59 +49,10 @@ consoleHandler = logging.StreamHandler(stdout)  # set streamhandler to stdout
 consoleHandler.setFormatter(logFormatter)
 logger.addHandler(consoleHandler)
 
-
-exchange_name = os.environ.get("EXCHANGE_NAME")
-rabbitmq_host = os.environ.get("RABBITMQ_HOST")
-rabbitmq_user = os.environ.get("RABBITMQ_USER")
-rabbitmq_password = os.environ.get("RABBITMQ_PASSWORD")
-
 async def send_to_backends(message_dict):
     logger.info(json.dumps(message_dict, default=str))
     await es.index(index="logs", document=message_dict)
     return True
-    
-async def log_event(message: DeliveredMessage):
-    response = b64decode(message.body)
-    message_dict = json.loads(response.decode("utf-8"))
-    message_dict["from"] = "MESSAGE_BROKER"
-
-    try:
-        message_dict = LogsCreate(**message_dict).dict()
-        await send_to_backends(message_dict)
-    except Exception as e:
-        logger.error(json.dumps(message_dict), "not valid", e)
-
-    await message.channel.basic_ack(
-        message.delivery.delivery_tag
-    )
-
-
-async def consume(loop):
-    connection = await aiormq.connect("amqp://{}:{}@{}/".format(rabbitmq_user, rabbitmq_password, rabbitmq_host), loop=loop)
-    channel = await connection.channel()
-
-    await channel.basic_qos(prefetch_count=1)
-
-    await channel.exchange_declare(
-        exchange=exchange_name, exchange_type='direct'
-    )
-
-    declare = await channel.queue_declare(durable=True, auto_delete=True)
-    await channel.queue_bind(declare.queue, exchange_name, routing_key='logging')
-
-    await channel.basic_consume(declare.queue, log_event)
-
-
-@app.on_event('startup')
-def startup():
-    loop = asyncio.get_event_loop()
-    # use the same loop to consume
-    try:
-        asyncio.ensure_future(consume(loop))
-    except Exception as e:
-        logger.exception(e)
-        raise Exception("ERROR TO START CONSUMER")
-
 
 @app.get("/")
 async def root():
@@ -154,8 +99,6 @@ async def get_log(
         }
         query["range"]["timestamp"]["gte"] = from_date.isoformat()
         query["range"]["timestamp"]["lt"] = to_date.isoformat()
-      
-    
 
     if services or actions or models or coproductionprocess_ids or user_ids or team_ids:
         query["bool"] = { "must": [] }
